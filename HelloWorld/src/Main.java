f58989b3020ebec21f5ebb726344b2a12a6409f7
Lvista <532177525@qq.com>
2022-10-05 22:54:26 +0800

    create a new bin

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 167776589; // 00001010 00000000 00010001 01001101
+        int n = 167776512; // 00001010 00000000 00010001 00000000
+        System.out.println(Integer.toBinaryString(i & n)); // 167776512
+    }
+}
---
+++ basic-integer/src/com/itranswarp/learnjava/SumOfInteger.java
@@ -0,0 +1,14 @@
+package com.itranswarp.learnjava;
+
+/**
+ * Sum of integers.
+ */
+public class SumOfInteger {
+
+	public static void main(String[] args) {
+		int n = 100;
+		int sum = sum = (1 + n) * n / 2;
+		System.out.println(sum);
+	}
+
+}
