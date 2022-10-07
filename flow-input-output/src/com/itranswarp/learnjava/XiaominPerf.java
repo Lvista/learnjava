806b94dcbcc1a6c3857d7fb7aef1dd5b2363e62f
Lvista <532177525@qq.com>
2022-10-07 18:03:39 +0800

    Change a class name

--- flow-input-output/src/com/itranswarp/learnjava/XiaominPerf.java
+++ flow-input-output/src/com/itranswarp/learnjava/XiaominPerf.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 /**
  * 输入上次考试成绩（int）和本次考试成绩（int），然后输出成绩提高的百分比，保留两位小数位（例如，21.75%）
  */
-public class XiaomingPerfor {
+public class XiaominPerf {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
