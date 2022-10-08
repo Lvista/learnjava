b6adfc205e899473f4d201b995920f1e98474c21
Lvista <532177525@qq.com>
2022-10-08 12:49:12 +0800

    add gitignore
    ignore the HelloWorld file

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int i = 167776589; // 00001010 00000000 00010001 01001101
-        int n = 167776512; // 00001010 00000000 00010001 00000000
-        System.out.println(Integer.toBinaryString(i & n)); // 167776512
+        String s1 = null;
+        if (s1 != null && s1.equals("hello")) {
+            System.out.println("hello");
+        }
     }
 }
